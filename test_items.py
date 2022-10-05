7e126b2d6215aad8bda3b8fd8db0aea351715402
kiwi <sergeeva.n.yu@gmail.com>
2022-10-06 00:24:45 +0300

    commented time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button_exists(browser):
     browser.get(link)
-    time.sleep(10)
+    # time.sleep(30)
     try:
         button_exists = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     except NoSuchElementException:
